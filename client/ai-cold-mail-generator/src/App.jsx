44a9950c482938dd397d531f3837d3779e569f21
Muskan-771 <muskanvij771@gmail.com>
2026-05-16 14:29:18 +0530

    Client Setup

---
+++ client/ai-cold-mail-generator/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+ 
+  return (
+    <>
+     <p className="text-4xl font-extrabold text-blue-600 bg-white p-6 rounded-2xl shadow-lg border border-gray-200 transition-all hover:scale-105 duration-300">hello world!</p>
+    </>
+  )
+}
+
+export default App
